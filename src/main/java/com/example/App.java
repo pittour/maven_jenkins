288e35e37be1cb123f3f71a92fbb225a95192d65
Kévin B <kevinbillerach@yahoo.fr>
2023-07-21 12:01:01 +0200

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.sqlite.Connexion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Connexion.bddConnexion();
+    }
+}
---
+++ src/main/java/com/example/sqlite/Connexion.java
@@ -0,0 +1,23 @@
+package com.example.sqlite;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connexion {
+    public static void bddConnexion(){
+        System.out.println("Connexion à la bdd");
+        String url = "JDBC:sqlite::memory:";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            try(Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement();){
+                System.out.println("Connexion réussie");
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }     
+    }
+}
